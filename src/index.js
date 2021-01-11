fbf427ebbca4f6446b2d66cb969cd3a7ae5efd99
cummaster <me lol>
2021-01-11 11:40:01 +0200

    ex1.3

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const Header = (props) => {
 )
 }
 const Part = (props) => {
+
 return (
 <div>
   <p>part {props.part}, exercises {props.exercises}</p>
@@ -20,10 +21,9 @@ return (
 const Content = (props) => {
   return (
   <div>
-    <Part part={props.part1}
-      exercises={props.exercises1}/>
-    <Part exercises={props.exercises2} part={props.part2}/>
-    <Part exercises={props.exercises3} part={props.part3}/>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part  part={props.part2} exercises={props.exercises2}/>
+    <Part  part={props.part3} exercises={props.exercises3}/>
 
   </div>
   )
@@ -40,6 +40,37 @@ return (
   </div>
 )
 }
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+const partExercise = 'exercises'
+console.log(part1[partExercise])
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1.name}
+      part2={part2.name}
+      part3={part3.name}
+      exercises1={part1.exercises}
+      exercises2={part2.exercises}
+      exercises3={part3.exercises}/>
+      <Total total={part1.exercises+part2.exercises+part3.exercises}/>
+
+      </div>
+  )
+}
+/*
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -62,24 +93,7 @@ const App = () => {
       <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
-/*
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-  */
 }
-
+*/
 
 ReactDOM.render(<App />, document.getElementById('root'))
