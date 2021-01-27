f9af2421d948f8809d03c76eea339610f29abdb9
cummaster <me lol>
2021-01-27 17:35:36 +0200

    ex.1.8

--- src/index.js
+++ src/index.js
@@ -3,6 +3,49 @@ import ReactDOM from 'react-dom'
 import './index.css';
 
 
+// a proper place to define a component
+const Statistics = (props) => {
+  return (
+    <div>
+    <h1>Statistics</h1>
+    <p>good  {props.good}</p> 
+    <p>neutral  {props.neutral}</p>
+    <p>bad  {props.bad}</p>
+    <p>all {props.good + props.neutral + props.bad}</p>
+    <p>average {(props.good - props.bad) / (props.good + props.neutral +props.bad)}</p>
+      <p>percentage {(100 * props.good) / (props.good+props.neutral+props.bad)}%</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+       <h1>Give Feedback</h1>     
+    <button onClick={() => setGood(good + 1)}>
+    good
+  </button>
+  <button onClick={() => setNeutral(neutral + 1)}>
+    neutral
+  </button>
+  <button onClick={() => setBad(bad + 1)}>
+    bad
+  </button>
+  <Statistics good={good} neutral={neutral} bad={bad}/>
+  </div>
+
+  )
+}
+
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
+/*
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -31,7 +74,7 @@ const App = () => {
     </div>
   )
 }
-
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
+*/
