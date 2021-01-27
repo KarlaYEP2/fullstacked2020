659fe5735bb9425420e44e465315e023267c6021
cummaster <me lol>
2021-01-27 18:19:43 +0200

    ex.1.9

--- src/index.js
+++ src/index.js
@@ -34,15 +34,15 @@ const App = () => {
   return (
     <div>
        <h1>Give Feedback</h1>     
-    <button onClick={() => setGood(good + 1)}>
-    good
-  </button>
-  <button onClick={() => setNeutral(neutral + 1)}>
-    neutral
-  </button>
-  <button onClick={() => setBad(bad + 1)}>
-    bad
-  </button>
+       <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
   <Statistics good={good} neutral={neutral} bad={bad}/>
   </div>
 
@@ -59,7 +59,11 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+    <div>
+    <h1>Statistics</h1>
+    <Statistic text="good" value={props.good} />
+    <Statistic text="neutral" value={props.neutral}  />
+    <Statistic text="bad" value={}  />
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -85,4 +89,5 @@ const App = () => {
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
+
 */
